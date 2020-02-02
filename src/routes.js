051a4793a7518f9f42b42473302b51172c1e73f1
rubensojunior <jroliveirati@gmail.com>
2020-02-01 23:13:15 -0300

    adding session and recipient controllers

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 
 import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
+import RecipientController from './app/controllers/RecipientController'
 
 const routes = new Router()
 
 routes.post('/users', UserController.store)
+routes.post('/sessions', SessionController.store)
+routes.post('/recipients', RecipientController.store)
 
 export default routes
