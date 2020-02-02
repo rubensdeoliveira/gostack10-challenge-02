a421289ac50ad3c43013f7f31bc9476db7935f2c
rubensojunior <jroliveirati@gmail.com>
2020-02-02 00:15:23 -0300

    adding jwt authentication

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,10 +1,61 @@
+import * as Yup from 'yup'
 import Recipient from '../models/Recipient'
 
 class RecipientController {
   async store(req, res) {
-    const recipient = await Recipient.create(req.body)
+    const {
+      id,
+      name,
+      street,
+      neighborhood,
+      number,
+      cep,
+      city,
+      state,
+      complement,
+    } = await Recipient.create(req.body)
 
-    return res.json(recipient)
+    return res.json({
+      id,
+      name,
+      street,
+      neighborhood,
+      number,
+      cep,
+      city,
+      state,
+      complement,
+    })
+  }
+
+  async update(req, res) {
+    const recipient = await Recipient.findByPk(req.params.id)
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Destinatário não encontrado' })
+    }
+
+    const {
+      name,
+      street,
+      neighborhood,
+      number,
+      cep,
+      city,
+      state,
+      complement,
+    } = await recipient.update(req.body)
+
+    return res.json({
+      name,
+      street,
+      neighborhood,
+      number,
+      cep,
+      city,
+      state,
+      complement,
+    })
   }
 }
 
